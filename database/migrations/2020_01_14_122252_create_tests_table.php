da847847a213dbf607b0ed3a8af93e84edcca45c
David Mills <d.e.mills@stats4sd.org>
2020-01-14 12:31:53 +0000

    initial commit - basic Laravel 6 / Backpack 4 with test table

---
+++ database/migrations/2020_01_14_122252_create_tests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ngo');
+            $table->string('district');
+            $table->integer('served_bens')->unsigned();
+            $table->integer('served_bens_confirm')->unsigned()->nullable();
+            $table->boolean('confirmed')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_report');
+    }
+}
